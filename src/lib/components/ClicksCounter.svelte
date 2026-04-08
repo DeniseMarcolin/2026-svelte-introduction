d757019d083b4b277b1163a24528ec6c6d070cd0
DeniseMarcolin <denise.marcolin@mail.polimi.it>
2026-04-08 15:24:46 +0200

    make property derived

--- src/lib/components/ClicksCounter.svelte
+++ src/lib/components/ClicksCounter.svelte
@@ -1,7 +1,6 @@
 <script>
-    console.log("hello world")
-    let count = $state(0)
-    let doubled = $derived (count * 2)
+    let { initialCount: count = $bindable() } = $props()
+    let doubled = $derived(count * 2)
 </script>
 
 <h3>Clicks Counter</h3>
